acb9177c902e6ee7fb8257499f93b081f50164e6
Mon <raquindoza14@gmail.com>
2020-11-18 19:29:18 +0800

    twitter og

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,21 @@ export default function HomePage() {
         <meta name='description' content='Percent Gain Calculator' />
         <meta
           name='keywords'
-          content='Online Converters, Calculator, converter, profit calculator, converters alculators, online converters, online measurements, Adjusted Cost Base, Compound Interest calculator, pnl, profit, loss, gain, calculator, invest, investment, percent, percentage, stocks, cryptocurrency, bitcon, btc, eth, ethereum'
+          content='Online Converters, Calculator, free, converter, profit calculator, converters alculators, online converters, online measurements, Adjusted Cost Base, Compound Interest calculator, pnl, profit, loss, gain, calculator, invest, investment, percent, percentage, stocks, cryptocurrency, bitcon, btc, eth, ethereum'
         />
         <link rel='icon' href='/favicon.ico' />
-        <meta property='og:title' content='PNL gain calculator' />
+        <meta property='og:title' content='Free PNL gain calculator' />
         <meta property='og:description' content='Calculate your profit gains' />
         <meta property='og:image' content='/og.png' />
         <meta property='og:url' content='https://pnl-calculator.vercel.app/' />
+
+        <meta name='twitter:title' content='Free PNL gain calculator' />
+        <meta
+          name='twitter:description'
+          content='Calculate your profit gains'
+        />
+        <meta name='twitter:image' content='/og.png' />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <Home />
     </>

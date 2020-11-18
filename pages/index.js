cb3e9d4b96e9eca36d13f15ac8425439e8853588
Mon <raquindoza14@gmail.com>
2020-11-18 19:20:51 +0800

    favicon, head meta

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import Home from 'containers/Home';
+import Head from 'next/head';
 
 export default function HomePage() {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>Gain PNL Calculator</title>
+        <meta name='description' content='Percent Gain Calculator' />
+        <meta
+          name='keywords'
+          content='Online Converters, Calculator, converter, profit calculator, converters alculators, online converters, online measurements, Adjusted Cost Base, Compound Interest calculator, pnl, profit, loss, gain, calculator, invest, investment, percent, percentage, stocks, cryptocurrency, bitcon, btc, eth, ethereum'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Home />
+    </>
+  );
 }

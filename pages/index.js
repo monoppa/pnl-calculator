99ada51b5f59c9446fdb0a702c1b0ec8dfac946a
Mon <raquindoza14@gmail.com>
2020-11-18 19:26:38 +0800

    og

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ export default function HomePage() {
           content='Online Converters, Calculator, converter, profit calculator, converters alculators, online converters, online measurements, Adjusted Cost Base, Compound Interest calculator, pnl, profit, loss, gain, calculator, invest, investment, percent, percentage, stocks, cryptocurrency, bitcon, btc, eth, ethereum'
         />
         <link rel='icon' href='/favicon.ico' />
+        <meta property='og:title' content='PNL gain calculator' />
+        <meta property='og:description' content='Calculate your profit gains' />
+        <meta property='og:image' content='/og.png' />
+        <meta property='og:url' content='https://pnl-calculator.vercel.app/' />
       </Head>
       <Home />
     </>

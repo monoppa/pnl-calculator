9accf586d8cd3137a2944e1a88ee6cc635d40d26
mon <raquindoza14@gmail.com>
2022-06-21 23:15:43 +0800

    add pnl gains

--- containers/Home/Home.js
+++ containers/Home/Home.js
@@ -1,16 +1,36 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import NumberInput from 'components/NumberInput';
-
 import React from 'react';
 import {} from 'prop-types';
-import Image from 'next/image';
-
 import styles from './Home.module.css';
 import useMounted from 'hooks/useMounted';
 
+const useLocalState = (initialValue, cacheKey) => {
+  const [state, setState] = useState(initialValue);
+
+  useEffect(() => {
+    const cache = localStorage.getItem(cacheKey);
+
+    if (cache) {
+      setState(cache);
+    }
+  }, [cacheKey]);
+
+  const setStateCached = useCallback(
+    (value) => {
+      localStorage.setItem(cacheKey, value);
+      setState(value);
+    },
+    [cacheKey]
+  );
+
+  return [state, setStateCached];
+};
+
 const Home = () => {
-  const [initialPrice, setInitialPrice] = useState(0);
-  const [currentPrice, setCurrentPrice] = useState(0);
+  const [initialPrice, setInitialPrice] = useLocalState(0, 'initialPrice');
+  const [currentPrice, setCurrentPrice] = useLocalState(0, 'currentPrice');
+  const [capital, setCapital] = useLocalState(0, 'capital');
   const [mounted] = useMounted();
 
   const percentGain = useMemo(() => {
@@ -25,12 +45,18 @@ const Home = () => {
     return 0;
   }, [initialPrice, currentPrice]);
 
+  const gain = (function calculateGain() {
+    return capital * (percentGain / 100);
+  })();
+
   const handleInitialChange = (e) => {
-    setInitialPrice(Number(e.target.value));
+    const value = Number(e.target.value);
+    setInitialPrice(value);
   };
 
   const handleCurrentChange = (e) => {
-    setCurrentPrice(Number(e.target.value));
+    const value = Number(e.target.value);
+    setCurrentPrice(value);
   };
 
   return (
@@ -43,13 +69,22 @@ const Home = () => {
         <div className='w-full p-4'>
           <div className={styles.calculatorWrap}>
             <div className='flex flex-col items-center w-full lg:w-1/2'>
-              <div className='mb-8'>
+              {/* <div className='mb-8'>
                 <Image src='/moon.svg' width={200} height={200} />
-              </div>
+              </div> */}
 
-              <p className='font-bold text-gray-500'>PNL</p>
               {mounted && (
-                <p className={styles.result}>{percentGain.toFixed(2)} %</p>
+                <div className=''>
+                  <span className='text-center'>
+                    <p className='font-bold text-gray-500'>PNL %</p>
+                    <p className={styles.result}>{percentGain.toFixed(2)} %</p>
+                  </span>
+
+                  <span className='text-center'>
+                    <p className='font-bold text-gray-500'>PNL</p>
+                    <p className={styles.result}>{gain.toFixed(2)}</p>
+                  </span>
+                </div>
               )}
             </div>
 
@@ -57,15 +92,27 @@ const Home = () => {
 
             <div className='w-full space-y-8 lg:w-1/2'>
               <NumberInput
+                label='Capital'
+                placeholder='1,000'
+                value={capital > 0 ? capital : undefined}
+                onChange={(e) => {
+                  const value = Number(e.target.value);
+                  setCapital(value);
+                }}
+              />
+
+              <NumberInput
                 label='Initial price'
                 placeholder='50,000'
                 onChange={handleInitialChange}
+                value={initialPrice > 0 ? initialPrice : undefined}
               />
 
               <NumberInput
                 label='Current price'
                 placeholder='50,000'
                 onChange={handleCurrentChange}
+                value={currentPrice > 0 ? currentPrice : undefined}
               />
 
               <button className={styles.submit} type='submit'>
